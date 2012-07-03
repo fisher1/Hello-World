b5c91ea4b9a06a8557c2a6b990dade381082142d
fisher1 <svilen.grigorov97@gmail.com>
2012-07-03 13:41:54 +0300

    hello

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title></title>
+    <meta content="">
+    <style></style>
+  </head>
+  <body>
+  <?php
+  include("Animal.php");
+  $animal= new(Animal);
+  $animal->getresult(4,8);
+  ?>
+  </body>
+</html>
